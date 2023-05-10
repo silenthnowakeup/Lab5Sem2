1ad05e9e40fa7128d1493cfcb9ce1fc836d93c93
Stas <112703038+silenthnowakeup@users.noreply.github.com>
2023-05-10 05:07:48 +0300

    Update functions.c

--- Lab5Sem2/functions.c
+++ Lab5Sem2/functions.c
@@ -288,7 +288,7 @@ void findIP(HashTable* hashTable, const char* filename, const char* enterValue) 
                 free(tmpdomainName);
             }
 
-            free((const char*)tempDomainName);
+            free(tempDomainName);
             tempDomainName = NULL;
         }
 
